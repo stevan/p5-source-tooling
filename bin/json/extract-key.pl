59c4cf97d9d1ee332486a99d791380c6ba29ab09
Stevan Little <stevan.little@booking.com>
2016-05-11 10:12:19 +0200

    sorting and uniqueness stripping

--- bin/json/extract-key.pl
+++ bin/json/extract-key.pl
@@ -6,6 +6,7 @@ use warnings;
 use lib 'lib';
 
 use Getopt::Long ();
+use List::Util 1.45 ();
 
 use Code::Tooling::Util::JSON qw[ decode encode ];
 
@@ -13,14 +14,30 @@ our $DEBUG = 0;
 
 sub main {
 
-    my ($key, $sort);
+    my ($key, $sort, $uniq);
     Getopt::Long::GetOptions(
         'key=s'   => \$key,
-        'sort'    => \$sort,
+        'sort=s'  => \$sort,
+        'uniq'    => \$uniq,
         'verbose' => \$DEBUG
     );
 
     ($key) || die 'Must provide a key to group-by';
+
+    my ($ordering, $direction);
+    if ( $sort ) {
+        if ( $sort =~ /^(str|num)$/ ) {
+            $ordering  = $1;
+            $direction = 'asc';
+        }
+        elsif ( $sort =~ /^(str|num)\:(asc|desc)$/ ) {
+            $ordering  = $1;
+            $direction = $2;
+        }
+        else {
+            die "Unrecognized sort option format: $sort";
+        }
+    }
 
     my $input = join '' => <STDIN>;
     my $data  = decode( $input );
@@ -36,7 +53,21 @@ sub main {
         push @output => $datum->{$key};
     }
 
-    @output = sort @output if $sort;
+    if ( $sort ) {
+        @output = sort @output               if $ordering eq 'str';
+        @output = sort { $a <=> $b } @output if $ordering eq 'num';
+        @output = reverse @output            if $direction eq 'desc';
+    }
+
+    if ( $uniq ) {
+        if ( $ordering ) {
+            @output = List::Util::uniqstr( @output ) if $ordering eq 'str';
+            @output = List::Util::uniqnum( @output ) if $ordering eq 'num';
+        }
+        else {
+            @output = List::Util::uniq( @output );
+        }
+    }
 
     print encode( \@output );
 }
