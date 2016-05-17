326d11859f2f2f41c524157a9183a58276edf9fc
Stevan Little <stevan.little@booking.com>
2016-05-17 09:58:36 +0200

    move module info extraction into the Perl class, clean up the script a little with sigs and such

--- bin/perl/collect-module-info.pl
+++ bin/perl/collect-module-info.pl
@@ -5,6 +5,12 @@ use warnings;
 
 use lib 'lib';
 
+use experimental qw[
+    state
+    signatures
+    postderef
+];
+
 use Path::Class  ();
 use Getopt::Long ();
 use Data::Dumper ();
@@ -12,7 +18,9 @@ use Data::Dumper ();
 use PPI;
 use MetaCPAN::Client;
 
-use Code::Tooling::Util::JSON qw[ encode ];
+use Code::Tooling::Perl;
+
+use Importer 'Code::Tooling::Util::JSON'       => qw[ encode ];
 use Importer 'Code::Tooling::Util::FileSystem' => qw[ traverse_filesystem ];
 
 our $DEBUG = 0;
@@ -95,8 +103,7 @@ main && exit;
 
 # subs ....
 
-sub check_module_versions_against_metacpan {
-    my ($mcpan, %args) = @_;
+sub check_module_versions_against_metacpan ($mcpan, %args) {
 
     foreach my $module ( @{ $args{modules} } ) {
         warn "Going to fetch data about $module->{namespace}" if $DEBUG;
@@ -122,99 +129,17 @@ sub check_module_versions_against_metacpan {
     }
 }
 
+sub extract_module_info ($source, $acc) {
+    state $perl = Code::Tooling::Perl->new;
 
+    push @$acc => map {
+        $_->{rel_path} = Path::Class::Dir
+            ->new( $_->{path} )
+            ->relative( $ROOT )
+            ->stringify;
+        $_;
+    } $perl->extract_module_info( $source )->@*;
     return;
-}
-
-sub extract_module_version_information {
-    my ($e, $modules) = @_;
-
-    warn "Looking at '$e'" if $DEBUG;
-
-    my $doc = PPI::Document->new( $e->stringify );
-
-    (defined $doc)
-        || die 'Could not load document: ' . $e->stringify;
-
-    my $current;
-    $doc->find(sub {
-        my ($root, $node) = @_;
-
-        # if we have a current namespace, descend to find version ...
-        if ( $current ) {
-
-            # Must be a quote or number
-            $node->isa('PPI::Token::Quote')          or
-            $node->isa('PPI::Token::Number')         or return '';
-
-            # To the right is a statement terminator or nothing
-            my $t = $node->snext_sibling;
-            if ( $t ) {
-                $t->isa('PPI::Token::Structure') or return '';
-                $t->content eq ';'               or return '';
-            }
-
-            # To the left is an equals sign
-            my $eq = $node->sprevious_sibling        or return '';
-            $eq->isa('PPI::Token::Operator')         or return '';
-            $eq->content eq '='                      or return '';
-
-            # To the left is a $VERSION symbol
-            my $v = $eq->sprevious_sibling           or return '';
-            $v->isa('PPI::Token::Symbol')            or return '';
-            $v->content =~ m/^\$(?:\w+::)*VERSION$/  or return '';
-
-            # To the left is either nothing or "our"
-            my $o = $v->sprevious_sibling;
-            if ( $o ) {
-                $o->content eq 'our'             or return '';
-                $o->sprevious_sibling           and return '';
-            }
-
-            warn "Found possible version in '$current->{namespace}' in '$e'" if $DEBUG;
-
-            my $version;
-            if ( $node->isa('PPI::Token::Quote') ) {
-                if ( $node->can('literal') ) {
-                    $version = $node->literal;
-                } else {
-                    $version = $node->string;
-                }
-            } elsif ( $node->isa('PPI::Token::Number') ) {
-                if ( $node->can('literal') ) {
-                    $version = $node->literal;
-                } else {
-                    $version = $node->content;
-                }
-            } else {
-                die 'Unsupported object ' . ref($node);
-            }
-
-            warn "Found version '$version' in '$current->{namespace}' in '$e'" if $DEBUG;
-
-            # we've found it!!!!
-            $modules->[-1]->{meta}->{version} = $version;
-
-            undef $current;
-        }
-        else {
-            # otherwise wait for next package ...
-            return 0 unless $node->isa('PPI::Statement::Package');
-            $current = {
-                namespace => $node->namespace,
-                line_num  => $node->line_number,
-                path      => $e->stringify,
-                rel_path  => $e->relative( $ROOT->parent )->stringify,
-                meta      => {},
-            };
-
-            push @$modules => $current;
-
-            warn "Found package '$current->{namespace}' in '$e'" if $DEBUG;
-        }
-
-        return;
-    });
 }
 
 1;
--- lib/Code/Tooling/Perl.pm
+++ lib/Code/Tooling/Perl.pm
@@ -16,6 +16,94 @@ sub new ($class, %args) {
 
 # ...
 
+sub extract_module_info ($self, $source) {
+
+    my $doc = PPI::Document->new( $source->stringify );
+
+    (defined $doc)
+        || die 'Could not load document: ' . $source->stringify;
+
+    my ($current, @modules);
+    $doc->find(sub {
+        my ($root, $node) = @_;
+
+        # if we have a current namespace, descend to find version ...
+        if ( $current ) {
+
+            # Must be a quote or number
+            $node->isa('PPI::Token::Quote')          or
+            $node->isa('PPI::Token::Number')         or return '';
+
+            # To the right is a statement terminator or nothing
+            my $t = $node->snext_sibling;
+            if ( $t ) {
+                $t->isa('PPI::Token::Structure') or return '';
+                $t->content eq ';'               or return '';
+            }
+
+            # To the left is an equals sign
+            my $eq = $node->sprevious_sibling        or return '';
+            $eq->isa('PPI::Token::Operator')         or return '';
+            $eq->content eq '='                      or return '';
+
+            # To the left is a $VERSION symbol
+            my $v = $eq->sprevious_sibling           or return '';
+            $v->isa('PPI::Token::Symbol')            or return '';
+            $v->content =~ m/^\$(?:\w+::)*VERSION$/  or return '';
+
+            # To the left is either nothing or "our"
+            my $o = $v->sprevious_sibling;
+            if ( $o ) {
+                $o->content eq 'our'             or return '';
+                $o->sprevious_sibling           and return '';
+            }
+
+            warn "Found possible version in '$current->{namespace}' in '$source'" if $DEBUG;
+
+            my $version;
+            if ( $node->isa('PPI::Token::Quote') ) {
+                if ( $node->can('literal') ) {
+                    $version = $node->literal;
+                } else {
+                    $version = $node->string;
+                }
+            } elsif ( $node->isa('PPI::Token::Number') ) {
+                if ( $node->can('literal') ) {
+                    $version = $node->literal;
+                } else {
+                    $version = $node->content;
+                }
+            } else {
+                die 'Unsupported object ' . ref($node);
+            }
+
+            warn "Found version '$version' in '$current->{namespace}' in '$source'" if $DEBUG;
+
+            # we've found it!!!!
+            $modules[-1]->{meta}->{version} = $version;
+
+            undef $current;
+        }
+        else {
+            # otherwise wait for next package ...
+            return 0 unless $node->isa('PPI::Statement::Package');
+            $current = {
+                namespace => $node->namespace,
+                line_num  => $node->line_number,
+                path      => $source->stringify,
+                meta      => {},
+            };
+
+            push @modules => $current;
+
+            warn "Found package '$current->{namespace}' in '$source'" if $DEBUG;
+        }
+
+        return;
+    });
+
+    return \@modules;
+}
 
 sub critique ($self, $path, $query) {
 
