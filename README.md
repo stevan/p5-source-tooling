7fd851ae2e2bc9eca4fafe26ac1a45cac4136e74
Stevan Little <stevan.little@booking.com>
2016-06-02 19:02:09 +0200

    adding in base namespace module and updating README a bit

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Source (Code) Tooling for Perl 5
 
-WIP, please ignore. Basically this is just to make it easier for me to share between work and home computer.
+WIP, please ignore. Basically this is just to make
+it easier for me to share between work and home
+computer.
 
 ## Installation-(ish)
 
+Assuming you have a checkout and are within that
+directory you can just do the following:
 ```
 perlbrew install perl-5.22.2
 perlbrew use perl-5.22.2
---
+++ lib/Source/Tooling.pm
@@ -0,0 +1,37 @@
+package Source::Tooling;
+
+use v5.22;
+use warnings;
+
+our $VERSION   = '0.01';
+our $AUTHORITY = 'cpan:STEVAN';
+our $DEBUG     = 0;
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Source::Tooling - Source (Code) Tooling for Perl 5
+
+=head1 DESCRIPTION
+
+WIP, please ignore. Basically this is just to make it easier
+for me to share between work and home computer.
+
+=head2 Installation-(ish)
+
+Assuming you have a checkout and are within that
+directory you can just do the following:
+
+    perlbrew install perl-5.22.2
+    perlbrew use perl-5.22.2
+    cpanm --installdeps .
+
+Go get some coffee, this can take a while.
+
+=cut
