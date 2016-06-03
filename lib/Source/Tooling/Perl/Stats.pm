69536f7e76649cd8dc68db87d35f6d9bf9f9c48b
Stevan Little <stevan.little@booking.com>
2016-06-03 10:49:42 +0200

    just makes life easier

--- lib/Source/Tooling/Perl/Stats.pm
+++ lib/Source/Tooling/Perl/Stats.pm
@@ -4,6 +4,9 @@ use v5.22;
 use warnings;
 use experimental 'signatures';
 
+use PPI;
+use PPI::Dumper;
+
 our $VERSION     = '0.01';
 our $AUTHORITY   = 'cpan:STEVAN';
 our $DEBUG       = 0;
@@ -11,8 +14,11 @@ our $IS_ABSTRACT = 1;
 
 sub ppi;
 
+sub ppi_dump ($self) { PPI::Dumper->new( $self->ppi )->string }
+
 sub source     ($self) { $self->ppi->content }
 sub line_count ($self) { scalar split /\n/ => $self->source }
+
 
 1;
 
