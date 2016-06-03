441938285f35bb55eda31cf1a2a51f9153fc3ab0
Stevan Little <stevan.little@booking.com>
2016-06-03 16:04:17 +0200

    add line_number

--- lib/Source/Tooling/Perl/Stats.pm
+++ lib/Source/Tooling/Perl/Stats.pm
@@ -16,8 +16,9 @@ sub ppi;
 
 sub ppi_dump ($self) { PPI::Dumper->new( $self->ppi )->string }
 
-sub source     ($self) { $self->ppi->content }
-sub line_count ($self) { scalar split /\n/ => $self->source }
+sub source      ($self) { $self->ppi->content }
+sub line_count  ($self) { scalar split /\n/ => $self->source }
+sub line_number ($self) { $self->ppi->line_number }
 
 
 1;
