20cab6cc2d88bc6d8664fd24a25c739c733bf8a7
Stevan Little <stevan.little@booking.com>
2016-06-09 11:21:17 +0200

    improve this a little with signatures

--- bin/json/fmap.pl
+++ bin/json/fmap.pl
@@ -3,8 +3,10 @@
 use v5.22;
 use warnings;
 use experimental qw[
+    signatures
     current_sub
     postderef
+    say
 ];
 
 use lib 'lib';
@@ -26,7 +28,7 @@ sub main {
     (defined $f)
         || die 'You must pass a function to map';
 
-    my $src = '$f = sub { '.$f.'; }';
+    my $src = '$f = sub ($e) { '.$f.'; }';
 
     (eval $src && ref $f eq 'CODE')
         || die 'Unable to compile function: ' . $f . ' because: ' . $@;
@@ -37,7 +39,7 @@ sub main {
     (ref $data eq 'ARRAY')
         || die "Can only collate JSON arrays, not:\n$input";
 
-    my @output = map $f->(), @$data;
+    my @output = map $f->($_), @$data;
 
     print encode( \@output );
 }
