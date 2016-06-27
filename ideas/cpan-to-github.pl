f2022b3f96a60a62638bb77271ceb56c3ebe7d64
Stevan Little <stevan.little@gmail.com>
2016-06-27 15:27:09 -0400

    adding in another couple ideas

---
+++ ideas/cpan-to-github.pl
@@ -0,0 +1,27 @@
+#!perl
+
+use strict;
+use warnings;
+
+=pod
+
+Read list of PAUSE ids:
+
+    https://metacpan.org/source/BOOK/Acme-CPANAuthors-Booking-2015120201/lib/Acme/CPANAuthors/Booking.pm
+
+Find the Author object via MetaCPAN:
+
+    https://metacpan.org/pod/MetaCPAN::Client::Author
+
+Query the github API till we find a candidate:
+
+    https://developer.github.com/v3/users/
+    ex:
+        https://api.github.com/search/users?q=%C3%A6var
+        https://api.github.com/users/avar
+
+=cut
+
+1;
+
+__END__
---
+++ ideas/sublime-plugin-experiment.pl
@@ -0,0 +1,29 @@
+#!perl
+
+use strict;
+use warnings;
+
+=pod
+
+http://docs.sublimetext.info/en/latest/index.html
+
+http://sublimetext.info/docs/en/extensibility/completions.html
+
+http://docs.sublimetext.info/en/latest/extensibility/completions.html
+http://docs.sublimetext.info/en/latest/extensibility/syntaxdefs.html#scopes-and-scope-selectors
+
+http://www.sublimetext.com/docs/2/api_reference.html#sublime_plugin.EventListener
+
+http://docs.sublimetext.info/en/latest/reference/completions.html
+http://docs.sublimetext.info/en/latest/reference/api.html
+
+https://forum.sublimetext.com/t/how-can-i-add-my-own-autocomplete-words/5631/3
+
+http://www.sublimetext.com/docs/2/api_reference.html#sublime.View
+
+=cut
+
+1;
+
+__END__
+
