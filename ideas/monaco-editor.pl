a4d7bd7fccdeb524f4f11f7a1e1995c7e87f8fd6
Stevan Little <stevan.little@gmail.com>
2016-06-30 08:50:01 +0200

    more notes

---
+++ ideas/monaco-editor.pl
@@ -0,0 +1,20 @@
+#!perl
+
+use strict;
+use warnings;
+
+=pod
+
+https://code.visualstudio.com/docs/tools/samples
+
+https://github.com/Microsoft/monaco-editor/tree/master
+
+https://code.visualstudio.com/docs/extensions/example-language-server
+
+https://code.visualstudio.com/blogs/2016/06/27/common-language-protocol
+
+=cut
+
+1;
+
+__END__
