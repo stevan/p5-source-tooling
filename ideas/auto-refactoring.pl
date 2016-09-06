dba67f233da23d78fc77410ba85c0b62e2bd0eed
Stevan Little <stevan.little@booking.com>
2016-09-06 10:21:52 +0200

    more notes;

---
+++ ideas/auto-refactoring.pl
@@ -0,0 +1,16 @@
+#!perl
+
+use strict;
+use warnings;
+
+=pod
+
+https://github.com/Yelp/git-code-debt
+https://github.com/Yelp/undebt
+http://engineeringblog.yelp.com/2016/08/undebt-how-we-refactored-3-million-lines-of-code.html
+
+=cut
+
+1;
+
+__END__
